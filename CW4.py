3ea3b0f60e71d555e221298691ab04a4700bd655
ansaam <elaf_ef@hotmail.com>
2023-09-05 21:21:37 +0300

    تم حل الواجب 4

---
+++ CW4.py
@@ -0,0 +1,35 @@
+def my_name():
+    name="ansam"
+    print(f"my name is {name}")
+
+my_name()
+def my_meal(food,drink):
+    food="shawarma"
+    drink="awar galb"
+    print(f"I like to eat {food}while drinking {drink}")
+
+    my_meal("shawarma","awar galb")
+def cube (number):
+    return number **3
+print(cube(3))
+
+def by_three(number):
+    if number %3==0:
+        return cube(number)
+    else:
+        False
+        print(by_three(2))
+        
+        my name is ansam
+        I like to eat shawarma while drinking awar galb
+        
+   
+ 
+
+
+  
+
+ 
+    
+   
+
---
+++ by_three.py
@@ -0,0 +1,12 @@
+from CW4 import cube
+
+
+def by_three(number):
+    if number %3==0:
+        return cube(number)
+    else:
+        False
+        print(by_three(2))
+
+        my name is ansam
+        I like to eat shawarma while drinking awar
